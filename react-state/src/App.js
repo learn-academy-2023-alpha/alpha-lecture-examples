06cfa7196f014335a6b1ea0450c2b7bcd4ecb6ec
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2023-03-01 14:41:34 -0800

    React State lecture with notes

---
+++ react-state/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Tracker from './components/Tracker'
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Monthly Alpha Tracker</h1>
+      <h3>January</h3>
+      <Tracker />
+      <h3>February</h3>
+      <Tracker />
+      <h3>March</h3>
+      <Tracker />
+      <h3>April</h3>
+      <Tracker />
+    </>
+  )
+}
+
+export default App
