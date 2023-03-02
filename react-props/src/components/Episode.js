40d1b83455969df57eee85d7c10a22e2d5fd2d4e
Sarah Proctor <sproctor950@gmail.com>
2023-03-02 10:47:50 -0800

    adds props project with compressed clone

---
+++ react-props/src/components/Episode.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Episode = (props) => {
+  // display component
+  return (
+    <>
+      <div className="card">{props.title}</div>
+    </>
+  )
+}
+
+export default Episode
