f8aa2d8e51500f529de3b523b784e2bff351e4a1
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2023-03-09 11:56:20 -0800

    updated pseudocode for Ruby blocks lecture

--- ruby_blocks.rb
+++ ruby_blocks.rb
@@ -12,64 +12,64 @@
 
 # EXAMPLE:
 
-# num = 1
-# while num <= 10
-#     p num
-#     num += 1
-# end
+num = 1
+while num <= 10
+    p num
+    num += 1
+end
 
 # Methods
 # methods can take an anonymous function, which in Ruby are called BLOCKS
 #  methods will be seen defined 2 ways: DO / END or { }
 # if there is a 1 liner - curlies otherwise DO /END
 
-# 5.times do
-#     # this will tell it how many 'times' to execute
-#     p 'Hello Alpha'
-#     # what will be returned
-# end
+5.times do
+    # this will tell it how many 'times' to execute
+    p 'Hello Alpha'
+    # what will be returned
+end
 
-# 8.times { p 'Hello World'}
-# # example of curlies
+8.times { p 'Hello World'}
+# example of curlies
 
-# num = 10
-# num.times do
-#     p 'Gene is awesome!'
-# end
+num = 10
+num.times do
+    p 'Gene is awesome!'
+end
 
 # EACH
 # iterator that takes an array — uses that as a start and end point
 # pipes are for passing parameters | |
 
-# numbers = [3, 4, 5, 6, 7]
-# numbers.each do |value|
-#     p value
-# end
+numbers = [3, 4, 5, 6, 7]
+numbers.each do |value|
+    p value
+end
 
-# numbers = [3, 4, 5, 6, 7]
-# numbers.each do |value|
-#     p value * 5
-# end
+numbers = [3, 4, 5, 6, 7]
+numbers.each do |value|
+    p value * 5
+end
 
 # RANGES
 # has a starting point
 # is separated by 2 dots
 # has an end point
 
-# 1..10 
+1..10 
 # # This is a range
 
-# first_range = 1..20
-# first_range.each do |value|
-#     p value
-# end
+first_range = 1..20
+first_range.each do |value|
+    p value
+end
 
-# alphabet_range = 'a'..'h'
-# alphabet_range.each do |value|
-#     p value
-# end
+alphabet_range = 'a'..'h'
+alphabet_range.each do |value|
+    p value
+end
 
-# p alphabet_range.to_a
+p alphabet_range.to_a
 # ["a", "b", "c", "d", "e", "f", "g", "h"]
 # .to_a will convert to an array
 
@@ -77,22 +77,22 @@
 # another iterator that returns the array of the same length
 # returns array
 
-# nums = 1..10
-# mapped = nums.map do |value|
-#     value * 2
-# end
-# p mapped
+nums = 1..10
+mapped = nums.map do |value|
+    value * 2
+end
+p mapped
 # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
-# nums = 1..10
-# mapped = nums.map do |value|
-#     if value % 2 == 0
-#         'even'
-#     else
-#         value
-#     end
-# end
-# p mapped
+nums = 1..10
+mapped = nums.map do |value|
+    if value % 2 == 0
+        'even'
+    else
+        value
+    end
+end
+p mapped
 # [1, "even", 3, "even", 5, "even", 7, "even", 9, "even"]
 
 nums = 1..10
@@ -133,8 +133,11 @@ p only_evens(1..10)
 
 def only_evens array
     array.select.with_index do |value, index|
+        # .with_index to return index of |passing index|
         value.even?
         index.even?
+        # returning the index 
     end
 end
 p only_evens(1..10)
+# [1, 3, 5, 7, 9]
