014da2fc5c5005fdd6f0f49735518bec720c6178
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2023-03-28 11:30:01 -0700

    Alpha rocks on with controllers, routes, and views. Lecture was awesome and lecture notes are located in README

--- controllers-routes-views/config/routes.rb
+++ controllers-routes-views/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # HTTP verb (get) / location / hash rocket / controller (Home) / method
+  get '/greeter' => 'home#greeter'
+  get '/joke' => 'home#joke'
+  get '/alpha' => 'home#alpha_cohort'
+  get '/landing' => 'home#landing'
+  root to: 'home#landing'
 end
